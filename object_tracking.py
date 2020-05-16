476ac85ecd8c45f882d02532ce44938d952d976c
harsh <harsh.gupta4@learner.manipal.edu>
2020-05-16 20:50:41 +0530

    object tracking

--- object_tracking.py
+++ object_tracking.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np 
 
-cap = cv2.VideoCapture('data\snakes.mp4')
+cap = cv2.VideoCapture('data\security.avi')
 #cap = cv2.VideoCapture(0) #uncomment the line to use your webcam 
 (camx,camy) = (640,520)
 
@@ -22,29 +22,29 @@ while(cap.isOpened()):
 
     _,thresh = cv2.threshold(blur,25,255,cv2.THRESH_BINARY)
 
-    #dilate = cv2.dilate(thresh,None,iterations=1)
-    #erode = cv2.erode(dilate,None,iterations=10)
+    dilate = cv2.dilate(thresh,None,iterations=5)
+    erode = cv2.erode(dilate,None,iterations=4)
 
-    mask_open = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,np.ones((5,5)))
-    mask_close = cv2.morphologyEx(mask_open,cv2.MORPH_GRADIENT,np.ones((10,10)))
-    mask = cv2.resize(mask_close,(512,256))
-    cv2.imshow('mask',mask)
+    # mask_open = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,np.ones((2,2)),iterations=3)
+    # mask_close = cv2.morphologyEx(mask_open,cv2.MORPH_GRADIENT,np.ones((2,2)),iterations=3)
+    #mask = cv2.resize(mask_close,(512,256))
+    #cv2.imshow('mask',erode)
 
-    contours , _ = cv2.findContours(mask_close,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours , _ = cv2.findContours(erode,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     #cv2.drawContours(frame1,contours,-1,(255,0,0),3)
     for contour in contours:
         (x,y,w,h) = cv2.boundingRect(contour)
-        if cv2.contourArea(contour)< 1000:
+        if cv2.contourArea(contour)< 800:
             continue
         cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,0,255),6)
         cv2.putText(frame1,"Movement",(20,40),cv2.FONT_HERSHEY_COMPLEX,2,(25,0,255),2)
 
-    frame = cv2.resize(frame1,(512,256)) 
-    frame_1 = cv2.resize(frame2,(512,256))
+    frame = cv2.resize(frame1,(600,400)) 
+    
     cv2.imshow("tracking",frame)
     
-    cv2.imshow("original",frame_1)
+    #cv2.imshow("original",frame_1)
     frame1 = frame2
 
     ret,frame2 = cap.read()
