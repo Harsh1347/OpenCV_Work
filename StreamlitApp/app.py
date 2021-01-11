968d76317aa10905c3eec1a944f1430b7116ceec
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2021-01-11 11:36:45 +0530

    OpenCV WebApp

---
+++ StreamlitApp/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import cv2 as cv
+st.set_page_config(page_title='OpenCV', layout='wide',
+                   initial_sidebar_state='collapsed')
+
+st.title("OpenCV Application")
